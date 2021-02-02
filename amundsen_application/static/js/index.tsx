06b22af57f86f7ea0756ea2e9007d3562d745b44
jere <jere.manni@gmail.com>
2021-02-02 11:32:47 +0200

    added login route and file

--- amundsen_application/static/js/index.tsx
+++ amundsen_application/static/js/index.tsx
@@ -23,6 +23,7 @@ import SearchPage from './pages/SearchPage';
 import ProfilePage from './pages/ProfilePage';
 import TableDetail from './pages/TableDetailPage';
 import TestProfilePage from './pages/TestProfilePage';
+import Login from './pages/Login';
 
 import Preloader from './components/common/Preloader';
 import Footer from './components/Footer';
@@ -59,6 +60,7 @@ ReactDOM.render(
             <Route path="/user/:userId" component={ProfilePage} />
             <Route path="/404" component={NotFoundPage} />
             <Route path="/profile/:person_id" component={TestProfilePage} />
+            <Route path="/login" component={Login} />
             <Route path="/" component={HomePage} />
 
           </Switch>
