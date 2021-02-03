967f75beb51f69fa9f682c159c9ac92964a3920d
jere <jere.manni@gmail.com>
2021-02-03 18:19:21 +0200

    more changes to profilepage styling

--- amundsen_application/static/js/pages/TestProfilePage/ProfileBox.tsx
+++ amundsen_application/static/js/pages/TestProfilePage/ProfileBox.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
 
 const ProfileBox = ({ person }) => {
-  const jobInfo = () => {
-    if (person.jobs.length == 0) {return person.headline}
-    else return person.jobs[0][0] + " - " + person.jobs[0][0]
-    };
-  
-  return (
-    <div className="container_j">
-      <h1>{ person.name }</h1>
-      <p>{ jobInfo }</p>
-    </div>
-  )}
+    if (person.jobs.length == 0) {
+      return (
+        <div className="container_j">
+          <h1>{ person.name }</h1>
+          <p>{ person.headline }</p>
+        </div>
+        )}
+    else {
+      return (
+        <div className="container_j">
+          <h1>{ person.name }</h1>
+          <p>{ person.jobs[0].title } - { person.jobs[0].company_name }</p>
+        </div>
+  )}}
 
 export default ProfileBox;
